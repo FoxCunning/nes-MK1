99fbd57deb66744d6b14da663188631320d49030
Fox <naprossen@gmail.com>
2023-07-17 21:45:01 +0100

    Bank split

---
+++ bank_06b.asm
@@ -0,0 +1,9 @@
+.segment "BANK_06b"
+; $D000-$DFFF
+; Then transferred to RAM ($7000-$7FFF)
+.setcpu "6502X"
+
+.feature org_per_seg
+.feature pc_assignment
+
+; -----------------------------------------------------------------------------
