bb92e433aeb19eada5cf904c2448205e1373a744
Fox <naprossen@gmail.com>
2024-01-13 11:09:57 +0000

    Moved sprite attribute bits to separate file

---
+++ attr_bits.asm
@@ -0,0 +1,283 @@
+	@attr_bits_00:
+	.byte $FF, $B7, $9C, $88, $22, $01, $40, $20
+	.byte $00, $00, $00, $20, $00, $00, $F0, $FF
+	@attr_bits_01:
+	.byte $06, $92, $13, $48, $18, $30, $08, $78
+	.byte $01, $C0, $04, $00, $00, $40, $00, $00
+	@attr_bits_02:
+	.byte $81, $0F, $4F, $E0, $9F, $80, $06, $20
+	.byte $00, $12, $00, $00, $00, $00, $FF, $FF
+	@attr_bits_03:
+	.byte $DF, $9C, $8F, $00, $00, $00, $00, $00
+	.byte $00, $C0, $04, $00, $3E, $22, $00, $02
+	@attr_bits_04:
+	.byte $FC, $FD, $47, $44, $00, $01, $00, $00
+	.byte $00, $00, $00, $00, $00, $00, $00, $F0
+	@attr_bits_05:
+	.byte $EF, $C1, $08, $46, $40, $20, $02, $00
+	.byte $00, $00, $F0, $11, $00, $00, $00, $00
+	@attr_bits_06:
+	.byte $BF, $3E, $7C, $60, $04, $8A, $80, $40
+	.byte $00, $00, $02, $00, $00, $80, $C0, $FF
+	@attr_bits_07:
+	.byte $77, $0F, $57, $00, $08, $00, $00, $22
+	.byte $00, $00, $07, $1C, $60, $98, $41, $38
+	@attr_bits_08:
+	.byte $FF, $63, $1D, $67, $02, $08, $06, $70
+	.byte $00, $00, $60, $00, $44, $14, $40, $FF
+	@attr_bits_09:
+	.byte $4B, $30, $0C, $00, $08, $80, $00, $30
+	.byte $21, $00, $02, $B6, $C4, $00, $00, $00
+	@attr_bits_0A:
+	.byte $BF, $26, $80, $11, $00, $04, $32, $08
+	.byte $00, $10, $00, $18, $00, $4C, $02, $F8
+	@attr_bits_0B:
+	.byte $0C, $B0, $00, $00, $00, $00, $00, $00
+	.byte $00, $00, $2C, $11, $00, $00, $00, $00
+	@attr_bits_0C:
+	.byte $FF, $F9, $3E, $64, $01, $92, $61, $00
+	.byte $00, $00, $00, $00, $00, $60, $00, $F0
+	@attr_bits_0D:
+	.byte $07, $03, $00, $00, $00, $00, $00, $00
+	.byte $00, $00, $00, $00, $00, $00, $00, $00
+
+	@attr_bits_1C:
+	.byte $FF, $9F, $FF, $7F, $FC, $FF, $FF, $FF
+	.byte $F9, $FF, $F8, $3F, $07, $00, $00, $C0
+	@attr_bits_1D:
+	.byte $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
+	.byte $FF, $FF, $FF, $0F, $00, $00, $40, $00
+	@attr_bits_1E:
+	.byte $FF, $FF, $FF, $FF, $FF, $FF, $FF, $DF
+	.byte $FF, $03, $02, $80, $00, $20, $FE, $FF
+	@attr_bits_1F:
+	.byte $FF, $CF, $FF, $3F, $FF, $DF, $FF, $7F
+	.byte $FF, $CF, $0F, $8F, $01, $00, $80, $00
+	@attr_bits_20:
+	.byte $FF, $FF, $FF, $3F, $FF, $3F, $FE, $9F
+	.byte $FF, $7F, $1E, $02, $C0, $20, $00, $FE
+	@attr_bits_21:
+	.byte $F9, $F9, $FF, $FF, $FF, $F9, $F7, $C3
+	.byte $DF, $04, $FE, $00, $FF, $FF, $3D, $04
+	@attr_bits_22:
+	.byte $87, $C7, $FC, $CF, $FF, $FE, $F9, $7F
+	.byte $EE, $7F, $FE, $3F, $7E, $7E, $EC, $30
+	@attr_bits_23:
+	.byte $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
+	.byte $7F, $88, $C7, $04, $10, $02, $C0, $00
+	@attr_bits_24:
+	.byte $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
+	.byte $FF, $FF, $FF, $00, $04, $00, $F8, $FF
+	@attr_bits_25:
+	.byte $FF, $FF, $FF, $FF, $DF, $FF, $F9, $FF
+	.byte $FE, $E1, $01, $C0, $01, $30, $60, $00
+	@attr_bits_26:
+	.byte $7F, $FC, $20, $01, $00, $00, $00, $00
+	.byte $00, $00, $00, $00, $00, $00, $00, $00
+	@attr_bits_27:
+	.byte $00, $00, $00, $00, $00, $00, $00, $00
+	.byte $00, $00, $00, $00, $00, $00, $00, $00
+	@attr_bits_28:
+	.byte $01, $00, $F2, $FF, $FF, $FF, $FF, $FF
+	.byte $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
+	@attr_bits_29:
+	.byte $0F, $09, $F9, $FF, $FF, $FD, $BF, $FF
+	.byte $FF, $FF, $FF, $FF, $FF, $7F, $FE, $7F
+	@attr_bits_2A:
+	.byte $99, $CC, $FF, $FF, $FF, $FF, $FF, $3F
+	.byte $00, $00, $00, $00, $00, $00, $00, $00
+	@attr_bits_2B:
+	.byte $00, $00, $00, $00, $00, $00, $00, $00
+	.byte $00, $00, $00, $00, $00, $00, $00, $00
+	@attr_bits_2C:
+	.byte $7F, $FB, $FF, $DF, $FF, $97, $FA, $E6
+	.byte $8F, $FF, $7E, $F4, $9E, $FC, $F7, $FF
+	@attr_bits_2D:
+	.byte $FF, $FF, $FF, $FB, $BF, $79, $7D, $FB
+	.byte $3F, $D4, $E2, $FF, $47, $2F, $06, $00
+	@attr_bits_2E:
+	.byte $FF, $6F, $FB, $FF, $FF, $13, $84, $40
+	.byte $02, $F0, $01, $20, $40, $F1, $39, $80
+	@attr_bits_2F:
+	.byte $9F, $F7, $F9, $DF, $99, $3D, $E0, $00
+	.byte $7F, $B7, $5B, $C0, $7F, $26, $17, $01
+	@attr_bits_30:
+	.byte $FF, $FF, $7F, $FF, $EF, $FA, $D7, $FB
+	.byte $7F, $FE, $FF, $00, $8D, $78, $77, $C4
+	@attr_bits_31:
+	.byte $0F, $C8, $FE, $BF, $FF, $FF, $7F, $72
+	.byte $FE, $3F, $CE, $CA, $FF, $73, $D6, $01
+	@attr_bits_32:
+	.byte $FF, $FF, $9F, $EB, $D3, $EB, $EC, $7E
+	.byte $E6, $33, $9F, $10, $20, $00, $20, $FF
+	@attr_bits_33:
+	.byte $FF, $0F, $7E, $F8, $C8, $07, $07, $E0
+	.byte $08, $BE, $21, $18, $B8, $32, $1F, $08
+	@attr_bits_34:
+	.byte $FF, $FF, $BC, $6A, $FB, $5D, $97, $E7
+	.byte $CB, $C8, $C1, $F6, $FD, $6F, $FF, $FF
+	@attr_bits_35:
+	.byte $FF, $FE, $4D, $BF, $C4, $49, $9E, $E7
+	.byte $24, $E9, $44, $92, $2F, $69, $1E, $68
+	@attr_bits_36:
+	.byte $FF, $EF, $FB, $99, $F8, $4C, $CC, $89
+	.byte $E7, $80, $A3, $12, $4E, $E5, $FF, $FF
+	@attr_bits_37:
+	.byte $FF, $3F, $FE, $F8, $CF, $67, $73, $C9
+	.byte $E4, $E6, $09, $4E, $27, $8F, $FF, $7F
+	@attr_bits_38:
+	.byte $FF, $E4, $EE, $1E, $00, $00, $00, $00
+	.byte $00, $00, $00, $00, $00, $00, $00, $00
+	@attr_bits_39:
+	.byte $00, $00, $00, $00, $00, $00, $00, $00
+	.byte $00, $00, $00, $00, $00, $00, $00, $00
+	@attr_bits_3A:
+	.byte $FF, $FF, $FF, $FF, $FF, $FF, $9F, $04
+	.byte $20, $00, $00, $00, $00, $00, $00, $FE
+	@attr_bits_3B:
+	.byte $19, $1F, $1F, $1E, $18, $78, $FE, $E3
+	.byte $3F, $3C, $C3, $18, $00, $00, $00, $08
+	@attr_bits_3C:
+	.byte $FF, $F8, $8F, $FF, $F1, $FF, $7C, $46
+	.byte $2E, $66, $10, $30, $10, $00, $00, $10
+	@attr_bits_3D:
+	.byte $9F, $10, $F8, $FF, $FF, $FF, $F7, $07
+	.byte $57, $00, $01, $00, $00, $00, $00, $00
+	@attr_bits_3E:
+	.byte $FF, $FF, $FF, $FF, $7F, $66, $E4, $03
+	.byte $00, $03, $00, $18, $00, $00, $00, $80
+	@attr_bits_3F:
+	.byte $FF, $F3, $7F, $FE, $99, $1D, $60, $78
+	.byte $00, $80, $01, $00, $00, $F0, $0F, $01
+	@attr_bits_40:
+	.byte $FF, $FF, $FD, $C7, $FF, $FC, $3F, $0F
+	.byte $18, $60, $00, $00, $00, $01, $C0, $FC
+	@attr_bits_41:
+	.byte $FF, $E4, $30, $C4, $01, $26, $C0, $73
+	.byte $80, $39, $9C, $C7, $F1, $F0, $78, $00
+	@attr_bits_42:
+	.byte $FF, $FF, $FF, $FF, $FF, $3F, $C0, $0F
+	.byte $30, $00, $00, $00, $80, $07, $00, $00
+	@attr_bits_43:
+	.byte $FF, $FF, $FF, $FF, $FF, $1D, $01, $20
+	.byte $00, $00, $00, $00, $C8, $1F, $00, $00
+	@attr_bits_44:
+	.byte $FF, $FF, $FF, $CF, $FD, $67, $0C, $00
+	.byte $00, $00, $00, $00, $00, $FE, $18, $F8
+	@attr_bits_45:
+	.byte $FF, $FF, $3F, $00, $00, $00, $60, $4C
+	.byte $0C, $00, $00, $00, $00, $00, $00, $00
+	@attr_bits_46:
+	.byte $FF, $FF, $FF, $FF, $FF, $18, $99, $08
+	.byte $09, $1D, $01, $FF, $FD, $3F, $C0, $FC
+	@attr_bits_47:
+	.byte $FF, $F7, $FF, $BF, $FF, $CF, $F9, $1F
+	.byte $0C, $7F, $C0, $31, $01, $F3, $FF, $7F
+	@attr_bits_48:
+	.byte $FF, $FF, $FF, $B7, $FF, $C0, $7F, $20
+	.byte $FF, $63, $CE, $F9, $FF, $9F, $FF, $FF
+	@attr_bits_49:
+	.byte $FF, $FF, $FF, $FF, $3F, $F3, $FF, $E1
+	.byte $FF, $FC, $E1, $A7, $3F, $CE, $FF, $07
+	@attr_bits_4A:
+	.byte $FF, $FF, $FF, $1F, $00, $00, $00, $F9
+	.byte $F3, $FF, $FF, $FF, $FF, $9B, $99, $FF
+	@attr_bits_4B:
+	.byte $FF, $FF, $FF, $FF, $FF, $FF, $07, $00
+	.byte $02, $FE, $FF, $FF, $FF, $FF, $03, $00
+	@attr_bits_4C:
+	.byte $FF, $FF, $FF, $FF, $FF, $3F, $8E, $20
+	.byte $7E, $FE, $FF, $FF, $FF, $CB, $FC, $FF
+	@attr_bits_4D:
+	.byte $FF, $FF, $FF, $FF, $1B, $7E, $87, $3F
+	.byte $19, $87, $E3, $FC, $1D, $FF, $3F, $07
+	@attr_bits_4E:
+	.byte $FF, $FF, $CF, $FD, $53, $81, $F0, $C0
+	.byte $60, $7E, $FE, $CF, $EF, $FB, $27, $FF
+	@attr_bits_4F:
+	.byte $FF, $FF, $FE, $DF, $F8, $C2, $C7, $FC
+	.byte $EF, $FF, $11, $FF, $67, $FE, $9F, $79
+	@attr_bits_50:
+	.byte $FF, $FF, $FF, $FF, $FF, $FF, $04, $7D
+	.byte $00, $90, $FF, $7F, $FC, $FF, $FF, $FF
+	@attr_bits_51:
+	.byte $FF, $FF, $FF, $8B, $B3, $98, $FD, $03
+	.byte $00, $00, $00, $00, $00, $00, $00, $00
+	@attr_bits_52:
+	.byte $51, $11, $B5, $16, $84, $E1, $67, $FB
+	.byte $BF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
+	@attr_bits_53:
+	.byte $06, $02, $9F, $8F, $3D, $F6, $F8, $1E
+	.byte $7E, $1E, $3F, $C4, $3F, $F2, $8F, $7F
+	@attr_bits_54:
+	.byte $EB, $08, $6C, $FD, $B8, $C6, $73, $12
+	.byte $67, $FF, $FF, $FF, $FF, $FF, $FF, $FF
+	@attr_bits_55:
+	.byte $14, $53, $95, $02, $64, $C0, $EC, $9F
+	.byte $FF, $FF, $FF, $FF, $0F, $F2, $FF, $7F
+	@attr_bits_56:
+	.byte $E1, $FD, $9D, $BF, $CF, $DF, $8F, $7F
+	.byte $FF, $5B, $04, $EA, $9F, $3F, $FF, $FF
+	@attr_bits_57:
+	.byte $90, $F3, $E6, $7D, $32, $FB, $CD, $79
+	.byte $BB, $1F, $FF, $FF, $FF, $FF, $FF, $7F
+	@attr_bits_58:
+	.byte $85, $65, $C9, $02, $D9, $70, $7F, $FE
+	.byte $FF, $FF, $FF, $FF, $FF, $77, $CE, $33
+	@attr_bits_59:
+	.byte $40, $2D, $11, $5E, $F0, $FF, $FE, $FF
+	.byte $FF, $FF, $FF, $FF, $FF, $FF, $7F, $00
+	@attr_bits_5A:
+	.byte $15, $25, $00, $00, $C0, $FF, $FF, $FF
+	.byte $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
+	@attr_bits_5B:
+	.byte $FF, $7F, $FE, $CB, $BC, $EB, $86, $F3
+	.byte $F7, $FF, $F1, $F3, $FF, $3F, $9F, $0F
+	@attr_bits_5C:
+	.byte $65, $29, $1D, $E4, $07, $FC, $7F, $FE
+	.byte $87, $FF, $FF, $FF, $FF, $FF, $FF, $FF
+	@attr_bits_5D:
+	.byte $7B, $6E, $D9, $20, $04, $3F, $F9, $FF
+	.byte $FF, $FF, $FF, $FF, $FF, $FF, $FF, $7F
+	@attr_bits_5E:
+	.byte $FF, $DB, $FF, $FF, $16, $00, $00, $00
+	.byte $00, $00, $00, $00, $00, $00, $00, $00
+	@attr_bits_5F:
+	.byte $00, $00, $00, $00, $00, $00, $00, $00
+	.byte $00, $00, $00, $00, $00, $00, $00, $00
+	@attr_bits_60:
+	.byte $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
+	.byte $FF, $FF, $FF, $1F, $00, $00, $00, $80
+	@attr_bits_61:
+	.byte $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
+	.byte $FF, $FF, $FF, $FF, $3F, $00, $00, $00
+	@attr_bits_62:
+	.byte $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
+	.byte $FF, $FF, $C6, $FF, $DF, $9F, $03, $07
+	@attr_bits_63:
+	.byte $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
+	.byte $0F, $00, $00, $FE, $FF, $7F, $80, $00
+	@attr_bits_64:
+	.byte $FF, $1F, $FF, $FC, $CF, $FF, $FF, $FF
+	.byte $03, $FF, $07, $FC, $E3, $FF, $FF, $03
+	@attr_bits_65:
+	.byte $FF, $FF, $FF, $FF, $FF, $CF, $FF, $FF
+	.byte $F3, $FF, $EF, $FF, $01, $00, $00, $03
+	@attr_bits_66:
+	.byte $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
+	.byte $FF, $FF, $FF, $FF, $03, $00, $00, $00
+	@attr_bits_67:
+	.byte $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
+	.byte $FF, $FF, $7F, $00, $00, $00, $10, $00
+	@attr_bits_68:
+	.byte $FF, $FF, $FF, $7F, $66, $92, $C4, $86
+	.byte $F2, $AF, $57, $82, $98, $8B, $FF, $FF
+	@attr_bits_69:
+	.byte $FF, $FF, $9F, $EC, $24, $7F, $81, $C0
+	.byte $1C, $78, $48, $46, $F2, $20, $10, $00
+	@attr_bits_6A:
+	.byte $FE, $DF, $FF, $84, $7F, $00, $00, $00
+	.byte $00, $00, $00, $00, $00, $00, $00, $00
+	@attr_bits_6B:
+	.byte $00, $00, $00, $00, $00, $00, $00, $00
+	.byte $00, $00, $00, $00, $00, $00, $00, $80
--- bank_01.asm
+++ bank_01.asm
@@ -3930,8 +3930,6 @@ sub_animate_sprites:
 ; palette 0 or 1
 rom_DAB4:
 	.byte $01, $02, $04, $08, $10, $20, $40, $80
-	; Potentially unused bytes (index is masked with and #$07)
-	;.byte $2C, $AC
 
 ; -----------------------------------------------------------------------------
 
@@ -3946,7 +3944,6 @@ tbl_frame_1_sfx_idx:
 	.byte $00, $00, $00, $00, $00, $00, $00, $00	; $20-$27
 	.byte $00, $00, $00, $00, $00, $00, $00, $00	; $28-$2F
 	.byte $00, $00, $00, $00, $00, $00, $00, $00	; $30-$37
-	; .byte $FF
 
 ; -----------------------------------------------------------------------------
 .export tbl_fighter_sprite_banks
@@ -4316,349 +4313,74 @@ sub_frozen_palette:
 
 ; Data pointers, indexed by CHR ROM bank number
 tbl_spr_attr_ptrs:
-	.word @rom_B0D8, @rom_B0E8		; Raiden
-	.word @rom_B0F8, @rom_B108
-	.word @rom_B118, @rom_B128
-	.word @rom_B138, @rom_B148
-	.word @rom_B158, @rom_B168
-	.word @rom_B178, @rom_B188
-	.word @rom_B198, @rom_B1A8
+	.word @attr_bits_00, @attr_bits_01		; Raiden (00-0D)
+	.word @attr_bits_02, @attr_bits_03
+	.word @attr_bits_04, @attr_bits_05
+	.word @attr_bits_06, @attr_bits_07
+	.word @attr_bits_08, @attr_bits_09
+	.word @attr_bits_0A, @attr_bits_0B
+	.word @attr_bits_0C, @attr_bits_0D
 	
-	.word @rom_B0D8, @rom_B0E8		; Stage BGs
-	.word @rom_B0D8, @rom_B0E8
-	.word @rom_B0D8, @rom_B0E8
-	.word @rom_B0D8, @rom_B0E8
-	.word @rom_B0D8, @rom_B0E8
-	.word @rom_B0D8, @rom_B0E8
-	.word @rom_B0D8, @rom_B0E8
+	.word @attr_bits_00, @attr_bits_01		; Stage BGs (0E-1B, unused)
+	.word @attr_bits_00, @attr_bits_01
+	.word @attr_bits_00, @attr_bits_01
+	.word @attr_bits_00, @attr_bits_01
+	.word @attr_bits_00, @attr_bits_01
+	.word @attr_bits_00, @attr_bits_01
+	.word @attr_bits_00, @attr_bits_01
 
-	.word @rom_B1B8, @rom_B1C8		; Sonya
-	.word @rom_B1D8, @rom_B1E8
-	.word @rom_B1F8, @rom_B208
-	.word @rom_B218, @rom_B228
-	.word @rom_B238, @rom_B248
-	.word @rom_B258, @rom_B268
-	.word @rom_B278, @rom_B288		; Shang Tsung
-	.word @rom_B298, @rom_B2A8
-	.word @rom_B2B8, @rom_B2C8		; Sub-Zero
-	.word @rom_B2D8, @rom_B2E8
-	.word @rom_B2F8, @rom_B308
-	.word @rom_B318, @rom_B328
-	.word @rom_B338, @rom_B348
-	.word @rom_B358, @rom_B368
-	.word @rom_B378, @rom_B388
-	.word @rom_B398, @rom_B3A8		; Liu Kang
-	.word @rom_B3B8, @rom_B3C8
-	.word @rom_B3D8, @rom_B3E8
-	.word @rom_B3F8, @rom_B408
-	.word @rom_B418, @rom_B428
-	.word @rom_B438, @rom_B448
-	.word @rom_B458, @rom_B468
-	.word @rom_B478, @rom_B488		; Johnny Cage
-	.word @rom_B498, @rom_B4A8
-	.word @rom_B4B8, @rom_B4C8
-	.word @rom_B4D8, @rom_B4E8
-	.word @rom_B4F8, @rom_B508
-	.word @rom_B518, @rom_B528		; Kano
-	.word @rom_B538, @rom_B548
-	.word @rom_B558, @rom_B568
-	.word @rom_B578, @rom_B588
-	.word @rom_B598, @rom_B5A8
-	.word @rom_B5B8, @rom_B5C8
-	.word @rom_B5D8, @rom_B5E8
-	.word @rom_B5F8, @rom_B608		; Goro
-	.word @rom_B618, @attr_fgtr_7
-	.word @rom_B638, @rom_B648
-	.word @rom_B658, @rom_B668
-	.word @rom_B678, @rom_B688		; Scorpion
-	.word @rom_B698, @rom_B6A8
+	.word @attr_bits_1C, @attr_bits_1D		; Sonya (1C-27)
+	.word @attr_bits_1E, @attr_bits_1F
+	.word @attr_bits_20, @attr_bits_21
+	.word @attr_bits_22, @attr_bits_23
+	.word @attr_bits_24, @attr_bits_25
+	.word @attr_bits_26, @attr_bits_27
 
-; -----------------------------------------------------------------------------
+	.word @attr_bits_28, @attr_bits_29		; Shang Tsung (28-2B)
+	.word @attr_bits_2A, @attr_bits_2B
+
+	.word @attr_bits_2C, @attr_bits_2D		; Sub-Zero/Scorpion (2C-39)
+	.word @attr_bits_2E, @attr_bits_2F
+	.word @attr_bits_30, @attr_bits_31
+	.word @attr_bits_32, @attr_bits_33
+	.word @attr_bits_34, @attr_bits_35
+	.word @attr_bits_36, @attr_bits_37
+	.word @attr_bits_38, @attr_bits_39
+
+	.word @attr_bits_3A, @attr_bits_3B		; Liu Kang (3A-45)
+	.word @attr_bits_3C, @attr_bits_3D
+	.word @attr_bits_3E, @attr_bits_3F
+	.word @attr_bits_40, @attr_bits_41
+	.word @attr_bits_42, @attr_bits_43
+	.word @attr_bits_44, @attr_bits_45
+
+	.word @attr_bits_46, @attr_bits_47		; Johnny Cage (46-51)
+	.word @attr_bits_48, @attr_bits_49
+	.word @attr_bits_4A, @attr_bits_4B
+	.word @attr_bits_4C, @attr_bits_4D
+	.word @attr_bits_4E, @attr_bits_4F
+	.word @attr_bits_50, @attr_bits_51
+
+	.word @attr_bits_52, @attr_bits_53		; Kano (52-5F)
+	.word @attr_bits_54, @attr_bits_55
+	.word @attr_bits_56, @attr_bits_57
+	.word @attr_bits_58, @attr_bits_59
+	.word @attr_bits_5A, @attr_bits_5B
+	.word @attr_bits_5C, @attr_bits_5D
+	.word @attr_bits_5E, @attr_bits_5F
+
+	.word @attr_bits_60, @attr_bits_61		; Goro (60-67)
+	.word @attr_bits_62, @attr_bits_63
+	.word @attr_bits_64, @attr_bits_65
+	.word @attr_bits_66, @attr_bits_67
+
+	.word @attr_bits_68, @attr_bits_69		; Scorpion (68-6B)
+	.word @attr_bits_6A, @attr_bits_6B
+
+; ----------------
 ; These are basically 128 bit masks with one bit for each of the 128 patterns
 ; in two 2KB CHR ROM banks
-
-	@rom_B0D8:
-	.byte $FF, $B7, $9C, $88, $22, $01, $40, $20
-	.byte $00, $00, $00, $20, $00, $00, $F0, $FF
-	@rom_B0E8:
-	.byte $06, $92, $13, $48, $18, $30, $08, $78
-	.byte $01, $C0, $04, $00, $00, $40, $00, $00
-	@rom_B0F8:
-	.byte $81, $0F, $4F, $E0, $9F, $80, $06, $20
-	.byte $00, $12, $00, $00, $00, $00, $FF, $FF
-	@rom_B108:
-	.byte $DF, $9C, $8F, $00, $00, $00, $00, $00
-	.byte $00, $C0, $04, $00, $3E, $22, $00, $02
-	@rom_B118:
-	.byte $FC, $FD, $47, $44, $00, $01, $00, $00
-	.byte $00, $00, $00, $00, $00, $00, $00, $F0
-	@rom_B128:
-	.byte $EF, $C1, $08, $46, $40, $20, $02, $00
-	.byte $00, $00, $F0, $11, $00, $00, $00, $00
-	@rom_B138:
-	.byte $BF, $3E, $7C, $60, $04, $8A, $80, $40
-	.byte $00, $00, $02, $00, $00, $80, $C0, $FF
-	@rom_B148:
-	.byte $77, $0F, $57, $00, $08, $00, $00, $22
-	.byte $00, $00, $07, $1C, $60, $98, $41, $38
-	@rom_B158:
-	.byte $FF, $63, $1D, $67, $02, $08, $06, $70
-	.byte $00, $00, $60, $00, $44, $14, $40, $FF
-	@rom_B168:
-	.byte $4B, $30, $0C, $00, $08, $80, $00, $30
-	.byte $21, $00, $02, $B6, $C4, $00, $00, $00
-	@rom_B178:
-	.byte $BF, $26, $80, $11, $00, $04, $32, $08
-	.byte $00, $10, $00, $18, $00, $4C, $02, $F8
-	@rom_B188:
-	.byte $0C, $B0, $00, $00, $00, $00, $00, $00
-	.byte $00, $00, $2C, $11, $00, $00, $00, $00
-	@rom_B198:
-	.byte $FF, $F9, $3E, $64, $01, $92, $61, $00
-	.byte $00, $00, $00, $00, $00, $60, $00, $F0
-	@rom_B1A8:
-	.byte $07, $03, $00, $00, $00, $00, $00, $00
-	.byte $00, $00, $00, $00, $00, $00, $00, $00
-	@rom_B1B8:
-	.byte $FF, $9F, $FF, $7F, $FC, $FF, $FF, $FF
-	.byte $F9, $FF, $F8, $3F, $07, $00, $00, $C0
-	@rom_B1C8:
-	.byte $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
-	.byte $FF, $FF, $FF, $0F, $00, $00, $40, $00
-	@rom_B1D8:
-	.byte $FF, $FF, $FF, $FF, $FF, $FF, $FF, $DF
-	.byte $FF, $03, $02, $80, $00, $20, $FE, $FF
-	@rom_B1E8:
-	.byte $FF, $CF, $FF, $3F, $FF, $DF, $FF, $7F
-	.byte $FF, $CF, $0F, $8F, $01, $00, $80, $00
-	@rom_B1F8:
-	.byte $FF, $FF, $FF, $3F, $FF, $3F, $FE, $9F
-	.byte $FF, $7F, $1E, $02, $C0, $20, $00, $FE
-	@rom_B208:
-	.byte $F9, $F9, $FF, $FF, $FF, $F9, $F7, $C3
-	.byte $DF, $04, $FE, $00, $FF, $FF, $3D, $04
-	@rom_B218:
-	.byte $87, $C7, $FC, $CF, $FF, $FE, $F9, $7F
-	.byte $EE, $7F, $FE, $3F, $7E, $7E, $EC, $30
-	@rom_B228:
-	.byte $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
-	.byte $7F, $88, $C7, $04, $10, $02, $C0, $00
-	@rom_B238:
-	.byte $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
-	.byte $FF, $FF, $FF, $00, $04, $00, $F8, $FF
-	@rom_B248:
-	.byte $FF, $FF, $FF, $FF, $DF, $FF, $F9, $FF
-	.byte $FE, $E1, $01, $C0, $01, $30, $60, $00
-	@rom_B258:
-	.byte $7F, $FC, $20, $01, $00, $00, $00, $00
-	.byte $00, $00, $00, $00, $00, $00, $00, $00
-	@rom_B268:
-	.byte $00, $00, $00, $00, $00, $00, $00, $00
-	.byte $00, $00, $00, $00, $00, $00, $00, $00
-	@rom_B278:
-	.byte $01, $00, $F2, $FF, $FF, $FF, $FF, $FF
-	.byte $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
-	@rom_B288:
-	.byte $0F, $09, $F9, $FF, $FF, $FD, $BF, $FF
-	.byte $FF, $FF, $FF, $FF, $FF, $7F, $FE, $7F
-	@rom_B298:
-	.byte $99, $CC, $FF, $FF, $FF, $FF, $FF, $3F
-	.byte $00, $00, $00, $00, $00, $00, $00, $00
-	@rom_B2A8:
-	.byte $00, $00, $00, $00, $00, $00, $00, $00
-	.byte $00, $00, $00, $00, $00, $00, $00, $00
-	@rom_B2B8:
-	.byte $7F, $FB, $FF, $DF, $FF, $97, $FA, $E6
-	.byte $8F, $FF, $7E, $F4, $9E, $FC, $F7, $FF
-	@rom_B2C8:
-	.byte $FF, $FF, $FF, $FB, $BF, $79, $7D, $FB
-	.byte $3F, $D4, $E2, $FF, $47, $2F, $06, $00
-	@rom_B2D8:
-	.byte $FF, $6F, $FB, $FF, $FF, $13, $84, $40
-	.byte $02, $F0, $01, $20, $40, $F1, $39, $80
-	@rom_B2E8:
-	.byte $9F, $F7, $F9, $DF, $99, $3D, $E0, $00
-	.byte $7F, $B7, $5B, $C0, $7F, $26, $17, $01
-	@rom_B2F8:
-	.byte $FF, $FF, $7F, $FF, $EF, $FA, $D7, $FB
-	.byte $7F, $FE, $FF, $00, $8D, $78, $77, $C4
-	@rom_B308:
-	.byte $0F, $C8, $FE, $BF, $FF, $FF, $7F, $72
-	.byte $FE, $3F, $CE, $CA, $FF, $73, $D6, $01
-	@rom_B318:
-	.byte $FF, $FF, $9F, $EB, $D3, $EB, $EC, $7E
-	.byte $E6, $33, $9F, $10, $20, $00, $20, $FF
-	@rom_B328:
-	.byte $FF, $0F, $7E, $F8, $C8, $07, $07, $E0
-	.byte $08, $BE, $21, $18, $B8, $32, $1F, $08
-	@rom_B338:
-	.byte $FF, $FF, $BC, $6A, $FB, $5D, $97, $E7
-	.byte $CB, $C8, $C1, $F6, $FD, $6F, $FF, $FF
-	@rom_B348:
-	.byte $FF, $FE, $4D, $BF, $C4, $49, $9E, $E7
-	.byte $24, $E9, $44, $92, $2F, $69, $1E, $68
-	@rom_B358:
-	.byte $FF, $EF, $FB, $99, $F8, $4C, $CC, $89
-	.byte $E7, $80, $A3, $12, $4E, $E5, $FF, $FF
-	@rom_B368:
-	.byte $FF, $3F, $FE, $F8, $CF, $67, $73, $C9
-	.byte $E4, $E6, $09, $4E, $27, $8F, $FF, $7F
-	@rom_B378:
-	.byte $FF, $E4, $EE, $1E, $00, $00, $00, $00
-	.byte $00, $00, $00, $00, $00, $00, $00, $00
-	@rom_B388:
-	.byte $00, $00, $00, $00, $00, $00, $00, $00
-	.byte $00, $00, $00, $00, $00, $00, $00, $00
-	@rom_B398:
-	.byte $FF, $FF, $FF, $FF, $FF, $FF, $9F, $04
-	.byte $20, $00, $00, $00, $00, $00, $00, $FE
-	@rom_B3A8:
-	.byte $19, $1F, $1F, $1E, $18, $78, $FE, $E3
-	.byte $3F, $3C, $C3, $18, $00, $00, $00, $08
-	@rom_B3B8:
-	.byte $FF, $F8, $8F, $FF, $F1, $FF, $7C, $46
-	.byte $2E, $66, $10, $30, $10, $00, $00, $10
-	@rom_B3C8:
-	.byte $9F, $10, $F8, $FF, $FF, $FF, $F7, $07
-	.byte $57, $00, $01, $00, $00, $00, $00, $00
-	@rom_B3D8:
-	.byte $FF, $FF, $FF, $FF, $7F, $66, $E4, $03
-	.byte $00, $03, $00, $18, $00, $00, $00, $80
-	@rom_B3E8:
-	.byte $FF, $F3, $7F, $FE, $99, $1D, $60, $78
-	.byte $00, $80, $01, $00, $00, $F0, $0F, $01
-	@rom_B3F8:
-	.byte $FF, $FF, $FD, $C7, $FF, $FC, $3F, $0F
-	.byte $18, $60, $00, $00, $00, $01, $C0, $FC
-	@rom_B408:
-	.byte $FF, $E4, $30, $C4, $01, $26, $C0, $73
-	.byte $80, $39, $9C, $C7, $F1, $F0, $78, $00
-	@rom_B418:
-	.byte $FF, $FF, $FF, $FF, $FF, $3F, $C0, $0F
-	.byte $30, $00, $00, $00, $80, $07, $00, $00
-	@rom_B428:
-	.byte $FF, $FF, $FF, $FF, $FF, $1D, $01, $20
-	.byte $00, $00, $00, $00, $C8, $1F, $00, $00
-	@rom_B438:
-	.byte $FF, $FF, $FF, $CF, $FD, $67, $0C, $00
-	.byte $00, $00, $00, $00, $00, $FE, $18, $F8
-	@rom_B448:
-	.byte $FF, $FF, $3F, $00, $00, $00, $60, $4C
-	.byte $0C, $00, $00, $00, $00, $00, $00, $00
-	@rom_B458:
-	.byte $FF, $FF, $FF, $FF, $FF, $18, $99, $08
-	.byte $09, $1D, $01, $FF, $FD, $3F, $C0, $FC
-	@rom_B468:
-	.byte $FF, $F7, $FF, $BF, $FF, $CF, $F9, $1F
-	.byte $0C, $7F, $C0, $31, $01, $F3, $FF, $7F
-	@rom_B478:
-	.byte $FF, $FF, $FF, $B7, $FF, $C0, $7F, $20
-	.byte $FF, $63, $CE, $F9, $FF, $9F, $FF, $FF
-	@rom_B488:
-	.byte $FF, $FF, $FF, $FF, $3F, $F3, $FF, $E1
-	.byte $FF, $FC, $E1, $A7, $3F, $CE, $FF, $07
-	@rom_B498:
-	.byte $FF, $FF, $FF, $1F, $00, $00, $00, $F9
-	.byte $F3, $FF, $FF, $FF, $FF, $9B, $99, $FF
-	@rom_B4A8:
-	.byte $FF, $FF, $FF, $FF, $FF, $FF, $07, $00
-	.byte $02, $FE, $FF, $FF, $FF, $FF, $03, $00
-	@rom_B4B8:
-	.byte $FF, $FF, $FF, $FF, $FF, $3F, $8E, $20
-	.byte $7E, $FE, $FF, $FF, $FF, $CB, $FC, $FF
-	@rom_B4C8:
-	.byte $FF, $FF, $FF, $FF, $1B, $7E, $87, $3F
-	.byte $19, $87, $E3, $FC, $1D, $FF, $3F, $07
-	@rom_B4D8:
-	.byte $FF, $FF, $CF, $FD, $53, $81, $F0, $C0
-	.byte $60, $7E, $FE, $CF, $EF, $FB, $27, $FF
-	@rom_B4E8:
-	.byte $FF, $FF, $FE, $DF, $F8, $C2, $C7, $FC
-	.byte $EF, $FF, $11, $FF, $67, $FE, $9F, $79
-	@rom_B4F8:
-	.byte $FF, $FF, $FF, $FF, $FF, $FF, $04, $7D
-	.byte $00, $90, $FF, $7F, $FC, $FF, $FF, $FF
-	@rom_B508:
-	.byte $FF, $FF, $FF, $8B, $B3, $98, $FD, $03
-	.byte $00, $00, $00, $00, $00, $00, $00, $00
-	@rom_B518:
-	.byte $51, $11, $B5, $16, $84, $E1, $67, $FB
-	.byte $BF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
-	@rom_B528:
-	.byte $06, $02, $9F, $8F, $3D, $F6, $F8, $1E
-	.byte $7E, $1E, $3F, $C4, $3F, $F2, $8F, $7F
-	@rom_B538:
-	.byte $EB, $08, $6C, $FD, $B8, $C6, $73, $12
-	.byte $67, $FF, $FF, $FF, $FF, $FF, $FF, $FF
-	@rom_B548:
-	.byte $14, $53, $95, $02, $64, $C0, $EC, $9F
-	.byte $FF, $FF, $FF, $FF, $0F, $F2, $FF, $7F
-	@rom_B558:
-	.byte $E1, $FD, $9D, $BF, $CF, $DF, $8F, $7F
-	.byte $FF, $5B, $04, $EA, $9F, $3F, $FF, $FF
-	@rom_B568:
-	.byte $90, $F3, $E6, $7D, $32, $FB, $CD, $79
-	.byte $BB, $1F, $FF, $FF, $FF, $FF, $FF, $7F
-	@rom_B578:
-	.byte $85, $65, $C9, $02, $D9, $70, $7F, $FE
-	.byte $FF, $FF, $FF, $FF, $FF, $77, $CE, $33
-	@rom_B588:
-	.byte $40, $2D, $11, $5E, $F0, $FF, $FE, $FF
-	.byte $FF, $FF, $FF, $FF, $FF, $FF, $7F, $00
-	@rom_B598:
-	.byte $15, $25, $00, $00, $C0, $FF, $FF, $FF
-	.byte $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
-	@rom_B5A8:
-	.byte $FF, $7F, $FE, $CB, $BC, $EB, $86, $F3
-	.byte $F7, $FF, $F1, $F3, $FF, $3F, $9F, $0F
-	@rom_B5B8:
-	.byte $65, $29, $1D, $E4, $07, $FC, $7F, $FE
-	.byte $87, $FF, $FF, $FF, $FF, $FF, $FF, $FF
-	@rom_B5C8:
-	.byte $7B, $6E, $D9, $20, $04, $3F, $F9, $FF
-	.byte $FF, $FF, $FF, $FF, $FF, $FF, $FF, $7F
-	@rom_B5D8:
-	.byte $FF, $DB, $FF, $FF, $16, $00, $00, $00
-	.byte $00, $00, $00, $00, $00, $00, $00, $00
-	@rom_B5E8:
-	.byte $00, $00, $00, $00, $00, $00, $00, $00
-	.byte $00, $00, $00, $00, $00, $00, $00, $00
-	@rom_B5F8:
-	.byte $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
-	.byte $FF, $FF, $FF, $1F, $00, $00, $00, $80
-	@rom_B608:
-	.byte $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
-	.byte $FF, $FF, $FF, $FF, $3F, $00, $00, $00
-	@rom_B618:
-	.byte $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
-	.byte $FF, $FF, $C6, $FF, $DF, $9F, $03, $07
-	@attr_fgtr_7:
-	.byte $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
-	.byte $0F, $00, $00, $FE, $FF, $7F, $80, $00
-	@rom_B638:
-	.byte $FF, $1F, $FF, $FC, $CF, $FF, $FF, $FF
-	.byte $03, $FF, $07, $FC, $E3, $FF, $FF, $03
-	@rom_B648:
-	.byte $FF, $FF, $FF, $FF, $FF, $CF, $FF, $FF
-	.byte $F3, $FF, $EF, $FF, $01, $00, $00, $03
-	@rom_B658:
-	.byte $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
-	.byte $FF, $FF, $FF, $FF, $03, $00, $00, $00
-	@rom_B668:
-	.byte $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
-	.byte $FF, $FF, $7F, $00, $00, $00, $10, $00
-	@rom_B678:
-	.byte $FF, $FF, $FF, $7F, $66, $92, $C4, $86
-	.byte $F2, $AF, $57, $82, $98, $8B, $FF, $FF
-	@rom_B688:
-	.byte $FF, $FF, $9F, $EC, $24, $7F, $81, $C0
-	.byte $1C, $78, $48, $46, $F2, $20, $10, $00
-	@rom_B698:
-	.byte $FE, $DF, $FF, $84, $7F, $00, $00, $00
-	.byte $00, $00, $00, $00, $00, $00, $00, $00
-	@rom_B6A8:
-	.byte $00, $00, $00, $00, $00, $00, $00, $00
-	.byte $00, $00, $00, $00, $00, $00, $00, $80
+.include "attr_bits.asm"
 
 ; -----------------------------------------------------------------------------
 
