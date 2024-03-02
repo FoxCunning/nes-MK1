a093605ad7b115fbd6eee9cf8fc719883d044ab5
Fox <naprossen@gmail.com>
2024-03-02 19:26:44 +0000

    Include build script for editor

---
+++ tools/build.bat
@@ -0,0 +1,16 @@
+REM @echo off
+setlocal
+
+pip install -r requirements.txt
+if %errorlevel% neq 0 goto Error
+
+::Replace with UPX installation path
+pyinstaller -F -w -s -i editor.ico --upx-dir F:\Programs\UPX\ animed.pyw
+if %errorlevel% neq 0 goto Error
+xcopy /C /Y /R .\img .\dist\img\
+
+timeout /t 3
+exit /b 0
+
+:Error
+pause
